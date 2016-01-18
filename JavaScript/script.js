60d8cedc581f9fc51464b5a235a682a34df512d9
Darnell Holder <darnelho1@gmail.com>
2016-01-18 14:33:03 -0800

    add functions for navigation and transitions

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,46 +10,104 @@ function BlogCreator(articleProto){
     articleStore.push(this);
 }
 
-BlogCreator.prototype.toHTML = function () {
-
-  $sectionWrapper = $('.sectionWrapper');
-
-  $sectionWrapper.append('Some text');
-};
+// BlogCreator.prototype.toHTML = function () {
+//
+//   $sectionWrapper = $('.sectionWrapper');
+//
+//   $sectionWrapper.append('Some text');
+// };
 
 
 articleProtos.forEach(function(article){
   blog = new BlogCreator(article);
 });
 
-articleStore.forEach(function(article){
-  article.toHTML();
-});
+// articleStore.forEach(function(article){
+//   article.toHTML();
+// });
 
 $(document).ready(function() {
+
   $('.blogArticleFooter').click(function(){
     console.log('Footer Click');
-    // $(this).parent().animate({
-    //   height: "100%"
-    // },1000);
-    $x = parseInt($(this).parent().css('height'));
-    $(this).parent().toggleClass('blogArticleGrowA');
-    $y = parseInt($(this).parent().css('height'));
-    $z = $y-$x;
-    $(this).parent().parent().scrollTop(0);
+    // $(this).parent().toggleClass('blogArticleGrowA');
 
-    console.log($z);
+    $(this).parent().animate({
+        "height": "92%",
+        "margin-top": "3.5%"
+      },2000);
+
+    $('.pageNav').delay(500).slideUp('slow');
+    $('.contentWrapper').width('100%');
+
     $(this).hide();
-
-    // console.log($(this).scrollTop());
-  //   setTimeout(function(){
-  //     console.log('hi');
-  // }, 2000);
-
   });
-  $('.navList li').click(function(){
-    console.log($(this).text());
+
+  $('.blogArticle').mouseleave(function() {
+
+    $('.pageNav').slideDown('slow');
+    $(this).animate({
+      height: "25%",
+      "margin-top": "20px"
+    },1000);
+
+    $(this).children().show();
   });
+
+
+    // $(this).parent().toggleClass('blogArticleGrowA');
+
+    // $(this).parent().parent().scrollTop(0);
+
+
+
+  // $('.navList li').click(function(){
+  //   console.log($(this).text());
+  // });
+
+  cleanpage = function(){
+    $(document).ready(function() {
+      $('.contentWrapper').children().fadeOut();
+
+      setTimeout(function () {
+        $('.pageNav').slideUp('slow');
+        $('.contentWrapper').width('100%')
+      }, 250);
+
+    });
+  };
+
+  $('.bannerNav li:contains("Info")').click(function(event) {
+    /* Act on the event */
+    cleanpage();
+
+
+    console.log("clicked");
+  });
+
+  $('.bannerNav li:contains("Contact")').click(function(event) {
+    /* Act on the event */
+    cleanpage();
+
+    console.log("clicked");
+  });
+
+  $('.bannerNav li:contains("Connect")').click(function(event) {
+    /* Act on the event */
+    cleanpage();
+
+    console.log("clicked");
+  });
+
+
+  $('.navButton').click(function(event) {
+    /* Act on the event */
+    console.log('button clicked');
+    $('.pageNav').slideDown(1000);
+    $('.blogArticle').fadeIn(1000);
+  });
+
+
   $('.bannerNav li').click(function(){
     console.log($(this));
   });
