8633d226f51aa99f9e694fc45345b65334372653
Darnell Holder <darnelho1@gmail.com>
2016-05-17 16:26:34 -0700

    Update articleProtos.js

--- JavaScript/articleProtos.js
+++ JavaScript/articleProtos.js
@@ -6,7 +6,7 @@ articleProtos = [
     category: 'Coding Projects',
     desc:'A Guessing Game for Code 201 ',
     image:'./images/guessinggame.png',
-    body:"<p>After much hemming and hawing I have finally decided to give this coding thing a real shot. I have tried to learn to code before through various attempts of teaching myself, to the failed attempts at bugging my developer friends to teach some software languge. This time I'm doing it for real. I have signed up for Code 201: Foundations of Software Development a course offered by <a href='http://www.codefellows.org'>Code Fellows</a>.</p> <p>The first assignment they are having us do is a JavaScript based Guessing Game using prompts and alerts for user input.</p> <div class=' flip imageWrapper'><div class='card'><div class='face front'> <img class = 'projectSS' src='./images/guessinggame.png'> <img class = 'projectSS' src='./images/guessinggame2.png'></div> <div class='face back'><pre><code class='javascript solarized_dark projectCode'>"+guessGame.toString()+"</code></pre></div></div></div>"
+    body:"<p>The first assignment they are having us do is a JavaScript based Guessing Game using prompts and alerts for user input.</p> <div class=' flip imageWrapper'><div class='card'><div class='face front'> <img class = 'projectSS' src='./images/guessinggame.png'> <img class = 'projectSS' src='./images/guessinggame2.png'></div> <div class='face back'><pre><code class='javascript solarized_dark projectCode'>"+guessGame.toString()+"</code></pre></div></div></div>"
   },
   {
     title: 'External Libraries',
